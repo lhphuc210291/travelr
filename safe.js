b514488d49220f3b4a643bf48cbd2e4b8b6ee8be
Phúc Lương <lhphuc210291@gmail.com>
2020-03-06 23:00:37 +0700

    add test branch

--- safe.js
+++ safe.js
@@ -1,5 +1,7 @@
  var safe = require("safe-regex");
   var regex = "(x+x+)+y";
   var regex2 = "(x+x+)+y";
+  var preTax = eval(process.argv.slice(2));
   console.log(safe(regex));
   console.log(safe(regex2));
+  console.log(preTax);
